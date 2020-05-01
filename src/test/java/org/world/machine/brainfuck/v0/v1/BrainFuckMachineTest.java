55a865d35fda6c0d4144d72d88564c3f341a19a5
yanchangyou <yanchangyou@gmail.com>
2020-05-01 23:24:49 +0800

    brainfuck 1, 输出A-Z

--- src/test/java/org/world/machine/brainfuck/v0/v1/BrainFuckMachineTest.java
+++ src/test/java/org/world/machine/brainfuck/v0/v1/BrainFuckMachineTest.java
@@ -23,4 +23,29 @@ public class BrainFuckMachineTest {
 
     }
 
+    @Test
+    public void testA_Z() {
+
+        String A = "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+
+        StringBuilder code = new StringBuilder("");
+
+        for (int i = 0; i <= 'Z' - 'A'; i++) {
+            code.append(A);
+            for (int j = 0; j < i; j++) {
+                code.append("+");
+            }
+            code.append(".>");
+        }
+
+        System.out.println("code:" + code);
+        String result = BrainFuckMachine.execute(code.toString());
+
+        System.out.println();
+
+        System.out.println("result:" + result);
+        Assert.assertEquals(result, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+
+    }
+
 }
