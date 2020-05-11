2d708cc7942dcffd2fa9989079a1b761a012eee2
yanchangyou <yanchangyou@gmail.com>
2020-05-12 00:06:37 +0800

    项目拆分成多个子maven工程

---
+++ turing-machine-bf/src/test/java/org/world/machine/bf/v0/v2/lib/BFMachineBasicLibTest.java
@@ -0,0 +1,107 @@
+package org.world.machine.bf.v0.v2.lib;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.world.machine.bf.v0.vx.lib.BFMachineBasicLib;
+
+public class BFMachineBasicLibTest {
+
+    @Test
+    public void testSwap() {
+        StringBuilder code = new StringBuilder();
+
+        int index1 = 1;
+        int index2 = 2;
+        BFMachineBasicLib.swap(code, index1, index2);
+
+        System.out.println(code);
+        Assert.assertEquals(code.toString(), "&>[->+<]&");
+    }
+
+    @Test
+    public void testSwapSameIndex() {
+        StringBuilder code = new StringBuilder();
+
+        int a = 1;
+        int b = 1;
+        BFMachineBasicLib.swap(code, a, b);
+
+        System.out.println(code);
+        Assert.assertEquals(code.toString(), "");
+    }
+
+    @Test
+    public void testSet() {
+        StringBuilder code = new StringBuilder();
+        int index = 1;
+        int value = 2;
+        BFMachineBasicLib.set(code, index, value);
+
+        Assert.assertEquals(code.toString(), "&>++&");
+    }
+
+    @Test
+    public void testReset() {
+
+        StringBuilder code = new StringBuilder();
+        int index = 1;
+        int length = 2;
+        BFMachineBasicLib.reset(code, index, length);
+        Assert.assertEquals(code.toString(), "&>_>_>&");
+    }
+
+    @Test
+    public void testOutput() {
+        StringBuilder code = new StringBuilder();
+        int index = 1;
+        BFMachineBasicLib.output(code, index);
+        Assert.assertEquals(code.toString(), "&>.&");
+    }
+
+    @Test
+    public void testTestSwap() {
+        StringBuilder code = new StringBuilder();
+
+        int fromIndex = 1;
+        int toIndex1 = 2;
+        int toIndex2 = 3;
+        BFMachineBasicLib.swap(code, fromIndex, toIndex1, toIndex2);
+
+        System.out.println(code);
+        Assert.assertEquals(code.toString(), "&>[->+>+<<]&");
+    }
+
+    @Test
+    public void testCopy() {
+        StringBuilder code = new StringBuilder();
+
+        int fromIndex = 1;
+        int toIndex = 2;
+        BFMachineBasicLib.copy(code, fromIndex, toIndex);
+
+        System.out.println(code);
+        Assert.assertEquals(code.toString(), "&>[->+<<+>]&&[->+<]&");
+    }
+
+    @Test
+    public void testDoubleSubOne() {
+        StringBuilder code = new StringBuilder();
+        int index1 = 1;
+        int index2 = 2;
+        BFMachineBasicLib.doubleSub(code, index1, index2);
+
+        System.out.println(code);
+        Assert.assertEquals(code.toString(), "&>[->-<]&");
+    }
+
+    @Test
+    public void testDoubleSubOneSameIndex() {
+        StringBuilder code = new StringBuilder();
+        int index1 = 1;
+        int index2 = 1;
+        BFMachineBasicLib.doubleSub(code, index1, index2);
+
+        System.out.println(code);
+        Assert.assertEquals(code.toString(), "");
+    }
+}
---
+++ turing-machine-lingxi/README.md
@@ -0,0 +1,1 @@
+## implementing turing machine
